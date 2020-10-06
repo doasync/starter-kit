ca0afa4c81065e691a65767c8d3feb529ac0ac97
doasync <doasync@gmail.com>
2020-10-06 17:57:50 +0300

    Remove redundant "features" folder

--- src/features/counter/model.ts
+++
@@ -1,6 +0,0 @@
-import { createEffect, createEvent, createStore } from 'effector';
-
-export const toggle = createEvent();
-export const tickFx = createEffect<void, void>();
-export const $timerWorking = createStore(false);
-export const $timer = createStore(0);

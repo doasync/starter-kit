e6a610c5426cb3ca0a5cdfe5bab490eb4bff1664
doasync <doasync@gmail.com>
2020-10-07 03:27:17 +0300

    TypeError: Cannot read property 'rs' of undefined

--- src/features/counter/tick-fx.ts
+++ src/features/counter/tick-fx.ts
@@ -1,4 +1,5 @@
-import { createEffect } from 'effector';
+/* eslint-disable unicorn/no-abusive-eslint-disable */
+import { createEffect, step } from 'effector';
 
 import { VoidFn } from '~/lib/effector-kit';
 
@@ -7,3 +8,35 @@ const tickDelay = 500;
 const tick: VoidFn = async () => new Promise((rs) => setTimeout(rs, tickDelay));
 
 export const tickFx = createEffect(tick);
+
+const enableFxOptions = <T>(fx: T): T => {
+  /* eslint-disable */
+
+  // @ts-expect-error: internal
+  const defaultCreate = fx.create;
+
+  // @ts-expect-error: internal
+  fx.create = (params, [options]) => defaultCreate([params, options]);
+
+  // @ts-expect-error: internal
+  fx.graphite.seq.shift(
+    step.compute({
+      // @ts-expect-error: internal
+      fn(upd, _, stack) {
+        if (!stack.parent) {
+          const {
+            params: [params, strategy],
+            req,
+          } = upd;
+          return { params, req, strategy };
+        }
+        return upd;
+      },
+    })
+  );
+  /* eslint-enable */
+
+  return fx;
+};
+
+enableFxOptions(tickFx);
